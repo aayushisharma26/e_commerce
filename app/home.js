28c4e02c99b876d197899d553c9bf45b02a38423
Aayushi Sharma <aayushisharma20@navgurukul.org>
2023-11-23 12:51:22 +0530

    Add CSS

--- app/home.js
+++ app/home.js
@@ -1,32 +1,28 @@
 "use client"
 import axios from "axios";
 import { useEffect, useState } from "react";
-let Home=()=>{
-  const [data,setData]=useState([]);
-  useEffect(()=>{
-    axios.get("https://fakestoreapi.com/products")
-    .then((response) => setData(response.data))
+
+let Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products").then((response) => setData(response.data));
   }, []);
 
-
-  return(
+  return (
     <>
-      <h1>Axios</h1>
-      {data.map((product)=>{
-        const{id,title,price,image}=product;
-
-        return(
-          <div className="card">
-          <h1>{id}</h1>
-          <p>{title}</p>
-          <h2>{price}</h2>
-          <h1><img src={image} alt={`Product ${id}`} /></h1>
-
-
+      <div className="row">
+        {data.map((product) => (
+          <div className="col" key={product.id}>
+            {/* <p>ID: {product.id}</p> */}
+            {/* <p>Title: {product.title}</p> */}
+            {/* <p>Price: {product.price}</p> */}
+            <img src={product.image} alt={product.title} />
           </div>
-        );
-      })}
+        ))}
+      </div>
     </>
   );
-}
+};
+
 export default Home;
