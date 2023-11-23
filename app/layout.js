619d3f861cb8cdefcee2b3ba3f99912972566450
Deepanjali <deepanjali20@navgurukul.org>
2023-11-23 12:20:26 +0530

    navbar error

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from './navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      <div>
+        <Navbar/>
+      </div>
+        
+
         {children}</body>
     </html>
   )
