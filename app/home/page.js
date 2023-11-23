717017fa836573391a889be53ea17299eb2718aa
Archana Singh <archana21@navgurukul.org>
2023-11-23 09:46:35 +0530

    Fatch data in homepage

--- app/home/page.js
+++ app/home/page.js
@@ -1,9 +1,32 @@
-import React from 'react'
+"use client"
+import axios from "axios";
+import { useEffect, useState } from "react";
+let Page=()=>{
+  const [data,setData]=useState([]);
+  useEffect(()=>{
+    axios.get("https://fakestoreapi.com/products")
+    .then((response) => setData(response.data))
+  }, []);
 
-function Home() {
-  return (
-    <div>Home Page</div>
-  )
+
+  return(
+    <>
+      <h1>Axios</h1>
+      {data.map((product)=>{
+        const{id,title,price,image}=product;
+
+        return(
+          <div className="card">
+          <h1>{id}</h1>
+          <p>{title}</p>
+          <h2>{price}</h2>
+          <h1><img src={image} alt={`Product ${id}`} /></h1>
+
+
+          </div>
+        );
+      })}
+    </>
+  );
 }
-
-export default Home;
+export default Page;
