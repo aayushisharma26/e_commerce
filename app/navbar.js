459e7f9bc076a4b0b7d7c723421c4287d024c4aa
Archana Singh <archana21@navgurukul.org>
2023-11-23 09:30:03 +0530

    code not working

--- app/navbar.js
+++ app/navbar.js
@@ -1,51 +1,7 @@
-// import React from 'react';
-// import Link from 'next/link';
-
-// function Navbar() {
-//   return (
-//     <div>
-//       <nav className="navbar navbar-expand-lg navbar-light bg-secondary" style={{ height: '80px', width: '100%' }}>
-//         <div style={{ marginRight: '10px' }}>
-//           <Link href="/">Home</Link>
-//         </div>
-        
-//         <div style={{ marginRight: '10px' }}>
-//           <Link href="/about">About</Link>
-//         </div>
-
-//         <div style={{ marginRight: '10px' }}>
-//           <Link href="/contact">Contact</Link>
-//         </div>
-
-//         <form className="d-flex" role="search">
-//           <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-//           <button className="btn btn-outline-success" type="submit">Search</button>
-//         </form>
-//       </nav>
-//     </div>
-//   );
-// }
-
-// export default Navbar;
-
-
-
-// components/Navbar.js
-
-
-
-
-
-// Import next/navigation instead of next/router
-
-"use client"
-import { useRouter } from 'next/navigation';
 import React from 'react';
 import Link from 'next/link';
 
 function Navbar() {
-  const router = useRouter();
-
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-secondary" style={{ height: '80px', width: '100%' }}>
@@ -70,21 +26,9 @@ function Navbar() {
   );
 }
 
-const NavItem = ({ path, currentPath, children }) => {
-  const isActive = currentPath === path;
-
-  return (
-    <div
-      style={{ marginRight: '10px', cursor: 'pointer', fontWeight: isActive ? 'bold' : 'normal' }}
-    >
-      <Link href={path}>
-        {children}
-      </Link>
-    </div>
-  );
-};
-
 export default Navbar;
+
+
 
 
 
