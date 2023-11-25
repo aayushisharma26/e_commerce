167d4397f4132f0b711fcbed9ce18e51cb295537
Aayushi Sharma <aayushisharma20@navgurukul.org>
2023-11-25 14:57:07 +0530

    Working on Searching

--- app/home.js
+++ app/home.js
@@ -1,22 +1,57 @@
 "use client"
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useEffect, useState } from "react";
 
-let Home = () => {
+const Home = () => {
   const [data, setData] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
 
   useEffect(() => {
-    axios.get("https://fakestoreapi.com/products").then((response) => setData(response.data));
+    axios.get("https://fakestoreapi.com/products").then((response) => {
+      setData(response.data);
+      // Extract unique categories from the data
+      const uniqueCategories = [...new Set(response.data.map((product) => product.category))];
+      setCategories(uniqueCategories);
+    });
   }, []);
+
+  useEffect(() => {
+    // Filter products based on the selected category
+    if (selectedCategory) {
+      const filteredProducts = data.filter((product) => product.category === selectedCategory);
+      setFilteredData(filteredProducts);
+    } else {
+      setFilteredData(data);
+    }
+  }, [selectedCategory, data]);
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
 
   return (
     <>
+      <div>
+        <label htmlFor="category">Select Category:</label>
+        <select id="category" onChange={handleCategoryChange} value={selectedCategory}>
+          <option value="">All Categories</option>
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
+
       <div className="row">
-        {data.map((product) => (
+        {filteredData.map((product) => (
           <div className="col" key={product.id}>
-            {/* <p>ID: {product.id}</p> */}
-            {/* <p>Title: {product.title}</p> */}
-            {/* <p>Price: {product.price}</p> */}
+            {/* Display other product information as needed */}
+            <p>ID: {product.id}</p>
+            <p>Title: {product.title}</p>
+            <p>Price: {product.price}</p>
             <img src={product.image} alt={product.title} />
           </div>
         ))}
@@ -26,3 +61,28 @@ let Home = () => {
 };
 
 export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/navbar.js
+++ app/navbar.js
@@ -19,7 +19,7 @@ function Navbar() {
 
         <form className="d-flex" role="search">
           <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success" type="submit">Search</button>
+          <button className="btn btn-outline-success" type="submit" >Search</button>
         </form>
       </nav>
     </div>
@@ -30,6 +30,43 @@ export default Navbar;
 
 
 
+
+
+
+
+
+
+// import React from 'react';
+// import Link from 'next/link';
+// import {useNavigate} from "react-router-dom"
+// function Navbar() {
+//   const navigate = useNavigate();
+
+//   return (
+//     <div>
+//       <nav className="navbar navbar-expand-lg navbar-light bg-secondary" style={{ height: '80px', width: '100%' }}>
+//         <div style={{ marginRight: '10px' }}>
+//           <Link href="/home">Home</Link>
+//         </div>
+        
+//         <div style={{ marginRight: '10px' }}>
+//           <Link href="/about">About</Link>
+//         </div>
+
+//         <div style={{ marginRight: '10px' }}>
+//           <Link href="/contact">Contact</Link>
+//         </div>
+
+//         <form className="d-flex" role="search">
+//           <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+//           <button className="btn btn-outline-success" type="submit" onClick={()=>navigate("/")} >Search</button>
+//         </form>
+//       </nav>
+//     </div>
+//   );
+// }
+
+// export default Navbar;
 
 
 
